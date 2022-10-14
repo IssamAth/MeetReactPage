6359db0de6526a9fdb83dce26dbd37503bc8d45f
Issame Aymen Athamena <samo.alger@gmail.com>
2022-10-15 00:58:10 +0100

    First commit - commiting all changes

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import Firstpage from './Components/Firstpage/Firstpage'
+
+export const App = () => {
+  return (
+    <div>
+        <Firstpage/>
+    </div>
+  )
+}
---
+++ src/Components/Firstpage/Firstpage.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import './firstpage.css'
+
+const Firstpage = () => {
+  return (
+    <div>Firstpage Issame</div>
+  )
+}
+
+export default Firstpage
