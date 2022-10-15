e94af2739707467465cc607cf4bef7b2bfcbc44f
Issame Aymen Athamena <samo.alger@gmail.com>
2022-10-15 19:25:56 +0100

    fourth commit - project almost done

--- src/Components/Firstpage/Firstpage.jsx
+++ src/Components/Firstpage/Firstpage.jsx
@@ -21,32 +21,40 @@ const Firstpage = () => {
           <div className='Image3'>
             <img src={IMG3} alt="" />
           </div>
-          <div className='right-top-img'>
-            <div className='Image5'>
-            <img src={IMG5} alt="" />
-            </div>
-            <div className='Image2'>
-              <img src={IMG2} alt="" />
-            </div>
+          <div className='right-top-img'> 
+            <img className='img5' src={IMG5} alt="" /> 
+            <img className='img2' src={IMG2} alt="" />     
           </div>
         </div>
         
-        <h1>Meet New Entrepreneurs</h1>
-        <h4>Meet is a new social media platform fir Entrepreneurs to connect and socialize</h4>
-        <button className='btn'>Get notified at launch</button>
-        <div className='twitter-follow'>
-            <img src={twitter} alt="" />
-            <h3>Follow us on Twitter</h3>
+        <h1>
+          Meeet &nbsp; New 
+          <br />
+          Entrepreneurs</h1>
+
+        <div class='text-two-img'>
+            <h4>Meeet is a new social media platform for <br />Entrepreneurs to connect and socialize.</h4>
+            <div className='Image6'>
+                <img src={IMG6} alt="" />
+            </div>
+            <div className='Image1'>
+              <img src={IMG1} alt="" />
+            </div>
         </div>
-        <div className='Image1'>
-          <img src={IMG1} alt="" />
-        </div>
-        <div className='Image4'>
-          <img src={IMG4} alt="" />
-        </div>
-        <div className='Image6'>
-            <img src={IMG6} alt="" />
-          </div>
+
+        <div className="inside-container">
+           <button className='btn'>Get notified at launch</button>
+          <div className='bt-pic-twt'>
+               <div className='twitter-follow'>
+                  <img src={twitter} alt="" />
+                  <h3>Follow us on Twitter</h3>
+               </div>
+                <div className='Image4'>
+                  <img src={IMG4} alt="" />
+                </div>  
+              </div>
+           </div>
+
     </div>
   )
 }
