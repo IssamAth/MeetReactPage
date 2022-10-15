daf025e434a5713fcdb7e3ac3d045962c84dbf56
Issame Aymen Athamena <samo.alger@gmail.com>
2022-10-15 01:59:54 +0100

    Second commit - commiting all changes

--- src/Components/Firstpage/Firstpage.jsx
+++ src/Components/Firstpage/Firstpage.jsx
@@ -1,9 +1,20 @@
 import React from 'react'
 import './firstpage.css'
+import twitter from '../../assets/Twitter.svg'
 
 const Firstpage = () => {
   return (
-    <div>Firstpage Issame</div>
+    <div className='container'>
+
+        <h1>Meet New Entrepreneurs</h1>
+        <h4>Meet is a new social media platform fir Entrepreneurs to connect and socialize</h4>
+        <button className='btn'>Get notified at launch</button>
+        <div>
+            <h3>Follow us on Twitter</h3>
+            <img src={twitter} alt="" />
+        </div>
+
+    </div>
   )
 }
 
